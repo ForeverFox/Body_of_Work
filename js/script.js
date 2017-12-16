d51e83a26e923356356f9f917628ba01413147d7
Forever Fox <cogden@mcad.edu>
2017-12-17 01:28:12 +0200

    JS
    Added image array, background-image change, and fade effect

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
-$("document").ready(function() {
-	$(function() {
-		var bow = $("#bowImage");
-		var bowBackgrounds = new Array(
-			"url(../bow/bodyofwork-header-1.jpg)",
-			"url(../bow/bodyofwork-header-2.jpg)",
-			"url(../bow/bodyofwork-header-3.jpg)",
-			"url(../bow/bodyofwork-header-bw.jpg)"			
-			);
-
-		var bowInitial = 0;
-
-		function newBackground() {
-			bow.css(
-				"backgroundImage",
-				bowBackgrounds[bowInitial = ++bowInitial%bowBackgrounds.length]
-				);
-
-		setTimeout(newBackground, 8000);
+$("document").ready(function() {	
+	var bow = $("#bowImage");
+// Creates an array of images to cycle through 
+	var bowBackgrounds = new Array(
+		"bodyofwork-header-0.jpg",
+		"bow-intro-01.jpg",
+		"bow-intro-10.jpg",
+		"bow-intro-11.jpg",
+		"bow-intro-03.jpg",
+		"bow-intro-02.jpg",
+		"bow-intro-04.jpg",
+		"bow-intro-08.jpg",
+		"bow-intro-13.jpg",
+		"bow-intro-12.jpg",
+		"bow-intro-09.jpg"												
+		);
+// Cycles through array
+		var i = 0;
+		bow.css("backgroundImage",
+			"url(bow/" + bowBackgrounds[i] + ")");
+		setInterval (function() {
+			i++;
+			if (i == bowBackgrounds.length) {
+			i = 0;
 		}
+// Adds timed fade in and out effects to background image transitions
+		bow.fadeOut(4000, function() {
+			$(this).css("background-image", "url(bow/" + bowBackgrounds[i] + ")");
+			$(this).fadeIn(4000);
+		});
+	}, 8000);
 
-		setTimeout(newBackground, 6000);
-		bow.css("bowBackgrounds", bowBackgrounds[0]);
-	});
+
 });
 
