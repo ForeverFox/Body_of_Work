3fe5b83711759b0a742ef9c95356592ad6d02da7
Forever Fox <cogden@mcad.edu>
2017-12-17 04:14:18 +0200

    Arranging the Gallery
    Setting up preloading and Magnific Popup

--- js/script.js
+++ js/script.js
@@ -28,8 +28,40 @@ $("document").ready(function() {
 			$(this).css("background-image", "url(bow/" + bowBackgrounds[i] + ")");
 			$(this).fadeIn(4000);
 		});
-	}, 8000);
+	}, 10000, "swing");
 
+// Magnific Popup Gallery
+/*	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	}); */
 
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			/*titleSrc: function(item) {
+				return item.el.attr('title') + '<small>Alex Ogden and Gili Estlin Hirsch</small>';
+			}*/
+		}
+	});
 });
 
